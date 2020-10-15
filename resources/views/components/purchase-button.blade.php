429f759834adbb55cd0b2a4bf7e4a831273d0b04
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-10-15 23:37:44 +0200

    remove paypal

--- resources/views/components/purchase-button.blade.php
+++ resources/views/components/purchase-button.blade.php
@@ -1,8 +1,8 @@
 @if (optional(Auth::user())->hasVerifiedEmail())
-    <a class="mx-auto cursor-pointer lg:mx-0 hover:underline gradient text-white font-bold rounded my-6 py-4 px-8 shadow-lg"
-            href="#">
+    <button class="mx-auto cursor-pointer lg:mx-0 hover:underline gradient text-white font-bold rounded my-6 py-4 px-8 shadow-lg">
         Purchase
-    </a>
+    </button>
+
 @elseif(Auth::check())
     <button class="mx-auto lg:mx-0 bg-gray-800 text-white font-bold rounded my-6 py-4 px-8 shadow-lg cursor-not-allowed opacity-50" disabled>
         Verify email
