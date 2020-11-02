cefd13e3786a566a3ad0307f74646c85bf3223e4
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-02 15:53:27 +0100

    skip donottrack users, track everyone!

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -1,2 +1,2 @@
-<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+<script data-skip-dnt="true" async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
 <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""/></noscript>
