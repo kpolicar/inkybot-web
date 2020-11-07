fa12e9ac07c5f5058ced79c41c449aed7506b044
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-07 15:36:35 +0100

    send one signal external user id

--- resources/views/partials/onesignal.blade.php
+++ resources/views/partials/onesignal.blade.php
@@ -5,6 +5,19 @@
         OneSignal.init({
             appId: "6a923a4c-c816-44f1-a098-67dbd40ac9cf",
         });
+
+        @auth
+        OneSignal.on('subscriptionChange', function(isSubscribed) {
+            if (isSubscribed) {
+                // The user is subscribed
+                //   Either the user subscribed for the first time
+                //   Or the user was subscribed -> unsubscribed -> subscribed
+                OneSignal.push(function () {
+                    OneSignal.setExternalUserId({{ Auth::user()->id }});
+                });
+            }
+        });
+        @endauth
     });
 </script>
 
