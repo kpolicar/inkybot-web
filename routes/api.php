a580df9b8814990ad6f9dd87c8f07788f6ad6d94
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-07 16:13:25 +0100

    add test notify api route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/notify', function (Request $request) {
+    \OneSignal::sendNotificationToExternalUser(
+        "This is a test notification.",
+        \App\Models\User::where('email', 'naltamer14@gmail.com')->first()->id,
+        $url = null,
+        $data = null,
+        $buttons = null,
+        $schedule = null
+    );
+});
+
 Route::get('/', function (ClientVersion $versions) {
     $last = $versions->latest();
 
