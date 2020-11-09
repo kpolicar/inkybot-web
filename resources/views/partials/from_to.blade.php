182e134268f47e7a62c678a561ad16d15c4c8fef
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-09 22:39:54 +0100

    version 5

---
+++ resources/views/partials/from_to.blade.php
@@ -0,0 +1,10 @@
+<div class="flex justify-between flex-wrap">
+    <div class="w-full text-center -mb-56 lg:mt-16 mt-10 z-10 text-gray-200">
+        <i class="fas fa-arrow-circle-right lg:text-6xl text-3xl border-gray border-dashed border rounded-full"></i>
+    </div>
+    <img src="{{ asset('item_before.png') }}" class="w-1/2 inline-block shadow-lg rounded my-4" alt="">
+    <img src="{{ asset('item_after.png') }}" class="w-1/2 inline-block shadow-lg rounded my-4" alt="">
+    <div class="w-full text-center xl:-mt-48 mt-0">
+        <img src="{{ asset('item_config.gif') }}" class="m-auto shadow-lg rounded-t rounded-b-xl my-4" >
+    </div>
+</div>
