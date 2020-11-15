e4f3dfbb60fd960249edaf0348bc5c29f98af88e
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-15 20:24:51 +0100

    version 7 release notes

--- app/ClientVersion.php
+++ app/ClientVersion.php
@@ -43,6 +43,11 @@ class ClientVersion
                 'code' => 'v0.6beta',
                 'name' => 'v0.6 Beta',
             ],
+            7 => [
+                'number' => 7,
+                'code' => 'v0.7beta',
+                'name' => 'v0.7 Beta',
+            ],
         ]);
     }
 
