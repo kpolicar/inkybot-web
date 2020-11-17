5d26461f778eb34827f3811605985d26be4df8cf
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-17 13:33:14 +0100

    add push notification for update command

---
+++ app/Console/Commands/SendUpdatePushNotification.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendUpdatePushNotification extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pushnotify:newupdate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send all users subscribed to push notifications a message for the new Inkybot Update';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        \OneSignal::sendNotificationToAll(
+            "A new version of Inkybot has just been released!",
+            $url = route('release', ['version' => 'latest']),
+            $data = null,
+            $buttons = null,
+            $schedule = null,
+            $headings = "Inkybot Update"
+        );
+
+        return 0;
+    }
+}
