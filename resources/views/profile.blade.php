2ec9919e97b0595ddd7ae3fe1d2040c0d48e3ead
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-11-21 15:22:43 +0100

    increase early referral reward

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -123,7 +123,8 @@
                         <div class="md:w-3/5">
                             <p class="text-gray-800">{{ route('register', ['ref' => request()->user()->referral_code]) }}</p>
                             <p class="py-2 text-sm text-gray-600">
-                                each referred user that purchases subscription will grant you 2 extra days of subscription
+                                each referred user that purchases subscription will grant you <strike class="text-gray-500">2</strike> <strong>7*</strong> extra days of subscription<br>
+                                *early referral bonus
                             </p>
                         </div>
                     </div>
