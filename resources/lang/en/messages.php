f602f77bf2e8f228bb92deeeb2679405887ae662
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-12-13 21:27:32 +0100

    translate welcome page hero

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'category' => 'The Dofus 2.0 Maging bot',
+    'heading' => 'Customize your items the way you want',
+    'subheading' => 'Tell us what stats you want on your items and we\'ll do the rest.',
+    'install_instructions' => 'Installation instructions',
+];
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,12 +2,14 @@
 
 @section('hero')
     <x-main-hero>
-        <h2 class="uppercase tracking-loose w-full">The Dofus 2.0 Maging bot</h2>
-        <h1 class="my-4 text-5xl font-bold leading-tight">Customize your items the way you want</h1>
-        <p class="leading-normal text-2xl mb-8">Tell us what stats you want on your items and we'll do the rest.</p>
+        <h2 class="uppercase tracking-loose w-full">{{ __('messages.category') }}</h2>
+        <h1 class="my-4 text-5xl font-bold leading-tight">{{ __('messages.heading') }}</h1>
+        <p class="leading-normal text-2xl mb-8">{{ __('messages.subheading') }}</p>
         <a href="{{ asset($download_asset) }}"
            download
-           class="inline-block mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded mt-6 py-4 px-8 shadow-lg">Download</a>
+           class="inline-block mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded mt-6 py-4 px-8 shadow-lg">
+            {{ __('common.download') }}
+        </a>
         <p class="my-2">
             <a href="{{ route('install') }}" class="text-gray-300 font-bold hover:underline">Installation instructions</a>
         </p>
