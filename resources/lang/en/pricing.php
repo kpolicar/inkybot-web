76ccb8d52e4c2f0af19249feb973a4553c456578
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-12-14 12:09:40 +0100

    translate features and pricing

---
+++ resources/lang/en/pricing.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'heading' => 'Pricing',
+    'description_prerelease_free' => 'Inkybot is currently <strong class="uppercase">free</strong> to use for everyone.',
+    'description_prerelease_appreciated' => 'Purchasing subscription before the official release is still very much appreciated.',
+    'description_prerelease_activated' => 'Your subscription will be activated once the pre-release period ends - perhaps with some added benefits.',
+    'description_thanks' => 'Thank you for your support.',
+
+    'package_free' => 'Open Beta',
+    'package_feature_unlimited_usage' => 'Unlimited use',
+    'package_feature_simple_maging' => 'Simple maging',
+    'package_feature_over_maging' => 'Overmaging',
+    'package_feature_exo_maging' => 'Exomaging',
+    'package_feature_magus_leveling' => 'Magus leveling',
+    'package_feature_in_development' => 'Features are under active development and have not yet been made available',
+];
--- resources/views/partials/pricing.blade.php
+++ resources/views/partials/pricing.blade.php
@@ -3,29 +3,29 @@
 
     <div class="container mx-auto px-2 pt-4 pb-2 text-gray-800">
 
-        <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">Pricing</h1>
+        <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800">{{ __('pricing.heading') }}</h1>
         <div class="w-full mb-4">
             <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
         <h2 class="w-full my-2 text-xl leading-tight text-center text-gray-800">
-            Inkybot is currently <strong class="uppercase">free</strong> to use for everyone.
-            Purchasing subscription before the official release is still very much appreciated.<br>
-            Your subscription will be activated once the pre-release period ends - perhaps with some added benefits.<br>
-            Thank you for your support.
+            {!! __('pricing.description_prerelease_free') !!}
+            {{ __('pricing.description_prerelease_appreciated') }}<br>
+            {{ __('pricing.description_prerelease_activated') }}<br>
+            {{ __('pricing.description_thanks') }}
         </h2>
 
         <div class="flex flex-col sm:flex-row justify-center pt-12 my-12 sm:my-4">
 
             <div class="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
                 <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-                    <div class="w-full p-8 text-3xl font-bold text-center">Open Beta</div>
+                    <div class="w-full p-8 text-3xl font-bold text-center">{{ __('pricing.package_free') }}</div>
                     <div class="h-1 w-full gradient my-0 py-0 rounded-t"></div>
                     <ul class="w-full text-center text-sm">
-                        <li class="border-b py-4">Unlimited use</li>
-                        <li class="border-b py-4">Simple maging</li>
-                        <li class="border-b py-4">Overmaging</li>
-                        <li class="border-b py-4">Exomaging</li>
-                        <li class="border-b py-4"><strike>Magus leveling</strike>*</li>
+                        <li class="border-b py-4">{{ __('pricing.package_feature_unlimited_usage') }}</li>
+                        <li class="border-b py-4">{{ __('pricing.package_feature_simple_maging') }}</li>
+                        <li class="border-b py-4">{{ __('pricing.package_feature_over_maging') }}</li>
+                        <li class="border-b py-4">{{ __('pricing.package_feature_exo_maging') }}</li>
+                        <li class="border-b py-4"><strike>{{ __('pricing.package_feature_magus_leveling') }}</strike>*</li>
                     </ul>
                 </div>
                 <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
@@ -33,7 +33,9 @@
                     <div class="flex items-center justify-center">
                         <a href="{{ asset($download_asset) }}"
                            download
-                            class="inline-block mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded my-6 py-4 px-8 shadow-lg">Download</a>
+                            class="inline-block mx-auto lg:mx-0 hover:underline gradient text-white font-bold rounded my-6 py-4 px-8 shadow-lg">
+                            {{ __('common.download') }}
+                        </a>
                     </div>
                 </div>
             </div>
@@ -41,7 +43,7 @@
         </div>
     </div>
 
-    <h3 class="w-full my-2 leading-tight text-center text-gray-800">* Features are under active development and have not yet been made available</h3>
+    <h3 class="w-full my-2 leading-tight text-center text-gray-800">* {{ __('pricing.package_feature_in_development') }}</h3>
 
 
 </section>
