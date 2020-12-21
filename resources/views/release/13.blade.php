4991bc641690664c2d3f1c01bc4805af1cc33dad
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-12-21 19:15:17 +0100

    fix date of official release

--- resources/views/release/13.blade.php
+++ resources/views/release/13.blade.php
@@ -8,7 +8,7 @@
 
         <h2 class="tracking-loose text-xl w-full font">v1.0</h2>
         <h1 class="mb-0 text-5xl font-bold leading-tight">Release notes</h1>
-        <h2 class="mb-4 font-bold tracking-loose text-lg w-full font">1st January 2020</h2>
+        <h2 class="mb-4 font-bold tracking-loose text-lg w-full font">1st January 2021</h2>
 
         <p class="leading-normal text-lg mb-2">
             Welcome to the official release of Inkybot!
