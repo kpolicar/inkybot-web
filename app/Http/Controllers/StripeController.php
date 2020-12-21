77f237b8f6aa8601af8be34cc56d4e23cddd4a9b
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-12-21 19:11:32 +0100

    charge 4€ instead of 5€ (stripe)

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -15,7 +15,7 @@ class StripeController extends Controller
             $user->createAsStripeCustomer();
 
         try {
-            $user->charge(500, $paymentId);
+            $user->charge(400, $paymentId);
         } catch (IncompletePayment $exception) {
             return response()->view('partials.payment.payment-error', compact('exception'));
         } catch (CardException $exception) {
