79b4797f7657cd9be58f90510bc09efb4612d971
Klemen Poličar <klemen.janez.policar@gmail.com>
2020-12-30 14:12:04 +0100

    remove gift route

--- routes/web.php
+++ routes/web.php
@@ -51,32 +51,6 @@ Route::get('/install', function (Request $request) {
     return view('install');
 })->name('install');
 
-Route::get('/gifts/christmas', function (Request $request) {
-    return view('christmas');
-})->name('gifts.christmas');
-
-Route::post('/gifts/christmas/claim', function (Request $request) {
-    $request->validate([
-        'confirmation' => 'accepted'
-    ], [
-        'confirmation.accepted' => 'Only good boys/girls can claim their gifts.',
-    ]);
-
-    $user = $request->user();
-    if (!$user->has_claimed_christmas_gift) {
-        $message = "You have successfully claimed your christmas gift! Happy holidays!";
-        $user->has_claimed_christmas_gift = true;
-        $user->subscribed_to = $user->subscribed_to->addDays(5);
-        $user->save();
-    } else {
-        $message = "Naughty, naughty! You have already claimed your christmas gift!";
-    }
-
-    return redirect()
-        ->route('profile')
-        ->with(['notification' => $message]);
-})->middleware('auth')->name('gifts.christmas');
-
 Route::post(
     'stripe/webhook',
     [WebhookController::class, 'handleWebhook']
