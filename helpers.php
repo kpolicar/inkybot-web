7bc6463cc1b5ba78a423de476604e8dc3f6bbfc1
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-02-07 15:55:11 +0100

    price also adjusts to 8€ after deadline

--- helpers.php
+++ helpers.php
@@ -5,8 +5,11 @@ use Carbon\Carbon;
 if (! function_exists('price')) {
     function price()
     {
+        $lastAvailableDateToBuySubCheaper = Carbon::parse('2021-03-01 00:00:00');
         $user = request()->user();
-        if ($user && Carbon::parse('2021-03-01 00:00:00')->isBefore($user->subscribed_to)) {
+        if (($user &&
+            $user->subscribed_to->isAfter($lastAvailableDateToBuySubCheaper)) ||
+            now()->isAfter($lastAvailableDateToBuySubCheaper)) {
             return 800;
         }
         return config('app.price');
