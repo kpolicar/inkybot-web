129a9d4b09e97586f74bd0a032620bf695842f69
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-02-26 14:52:41 +0100

    run passport purge at 3:00 at night

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('passport:purge')->daily();
+        $schedule->command('passport:purge')->dailyAt('3:00');
     }
 
     /**
