6565561c4fda9c01b087d80fc9d8a5478a1468df
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-01 19:16:53 +0100

    hall of fame engage, download asset patch4

--- resources/lang/fr/engage.php
+++ resources/lang/fr/engage.php
@@ -2,7 +2,7 @@
 
 return [
 
-    'heading' => 'Nous sommes enfin là!',
-    'subheading' => 'La première <strong class="uppercase">version majeure</strong> est là! Préparez vos équipements, les exos arrivent!',
+    'heading' => 'Hall of Fame',
+    'subheading' => 'Découvrez les histoires de réussite dans le <a href=":link" class="font-bold uppercase hover:underline">Hall of Fame</a> et voyez ce qu\'Inkybot apporte à la table.',
     'download' => 'Télécharger maintenant!',
 ];
