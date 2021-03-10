8776f9a36387e21c15718f6eb01aecaf9282bdaa
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-10 21:33:00 +0100

    remove dofus 2.0 -> becomes dofus 2

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'category' => 'The Dofus 2.0 Maging bot',
+    'category' => 'The Dofus 2 Maging bot',
     'heading' => 'Customize your items the way you want',
     'subheading' => 'Tell us what stats you want on your items and we\'ll do the rest.',
     'install_instructions' => 'Installation instructions',
