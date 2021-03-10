df9074100e81b5ca336659cd5164b44d5bf02c3e
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-03-10 21:24:08 +0100

    referrer rewards 2 instead of 5 (no more early rewards), add kamas keyword to site, add kamas expended data

--- resources/lang/en/meta.php
+++ resources/lang/en/meta.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'main_description' => 'Inkybot is a Dofus Maging Bot compatible with the latest version of the game. Configure the stats you want and let Inkybot do the rest.',
+    'main_description' => 'Inkybot is a Dofus Maging Bot compatible with the latest version of the game. Make kamas effortlessly. Configure the stats you want and let Inkybot do the rest.',
     'home' => 'Home',
     'invite_description' => ':name is inviting you to check out Inkybot, a Dofus Maging Bot!',
     'invite_anonymous_description' => 'You have been invited to check out Inkybot, a Dofus Maging Bot!',
