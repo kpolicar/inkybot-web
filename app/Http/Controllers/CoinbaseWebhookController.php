ebdd30c29aa4dc66e8075f2a9cee77cfcd1e4e00
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-04-13 18:17:08 +0200

    proper error handling on coinbase webhook

--- app/Http/Controllers/CoinbaseWebhookController.php
+++ app/Http/Controllers/CoinbaseWebhookController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Events\CoinbaseWebhookReceived;
+use CoinbaseCommerce\Exceptions\InvalidResponseException;
+use CoinbaseCommerce\Exceptions\SignatureVerificationException;
 use DB;
 use App\Events\UserPurchasedSubscription;
 use App\Models\User;
@@ -16,14 +18,25 @@ use Illuminate\Support\Str;
 use Laravel\Cashier\Events\WebhookHandled;
 use Laravel\Cashier\Events\WebhookReceived;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class CoinbaseWebhookController extends Controller
 {
-
     public function handleWebhook(Request $request)
     {
-        $signature = $request->header('X-CC-Webhook-Signature');
-        $event = Webhook::buildEvent($request->getContent(), $signature, config('services.coinbase.webhook_secret'));
+        try {
+            $signature = $request->header('X-CC-Webhook-Signature');
+            $payload = $request->getContent();
+
+            if (!is_string($signature))
+                throw new SignatureVerificationException($signature, $payload);
+            $event = Webhook::buildEvent($payload, $signature, config('services.coinbase.webhook_secret'));
+        } catch (InvalidResponseException $exception) {
+            throw new BadRequestHttpException($exception->getMessage(), $exception);
+        } catch (SignatureVerificationException $exception) {
+            throw new AccessDeniedHttpException('No signatures found matching the expected signature for payload', $exception);
+        }
 
         CoinbaseWebhookReceived::dispatch($event);
 
