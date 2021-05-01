330834672f65aa07329fe51e22358c25e9b5736a
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-01 19:55:45 +0200

    fix profile page when there is no previous activity or today's activity

--- resources/views/partials/statistics-previous.blade.php
+++ resources/views/partials/statistics-previous.blade.php
@@ -1,5 +1,5 @@
 @php($maging = \Auth::user()->maging()->notTodays()->latest()->first())
-@php($magingsOnSameDay = \Auth::user()->maging()->whereDate('created_at', $maging->created_at)->latest()->get())
+@php($magingsOnSameDay = !$maging ? [] : \Auth::user()->maging()->whereDate('created_at', $maging->created_at)->latest()->get())
 
 <div class="p-8">
     <div class="md:flex flex-wrap">
@@ -8,6 +8,7 @@
                 {{ __('profile.activity_previous') }}
             </label>
 
+            @if ($maging)
             <div class="w-1/2 md:w-full m-auto">
                 <input type="date"
                        disabled
@@ -15,12 +16,15 @@
                        class="cursor-not-allowed mt-2 mb-4 inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-black hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" />
 
             </div>
+            @endif
         </div>
         <div class="md:w-4/5">
             @forelse($magingsOnSameDay as $maging)
                 @include('partials/statistics', ['maging' => $maging])
             @empty
-                empty
+                <p class="text-gray-800">
+                    {{ __('profile.activity_none') }}
+                </p>
             @endforelse
         </div>
     </div>
