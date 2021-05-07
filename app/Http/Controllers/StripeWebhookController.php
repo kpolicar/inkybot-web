43d2ae1092c7693e5fcbc5d21e68b432632d5d5e
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-07 19:21:58 +0200

    store stripe & coinbase payments to database, store user stripe balance and show as credit on profile page

--- app/Http/Controllers/StripeWebhookController.php
+++ app/Http/Controllers/StripeWebhookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\PaymentSucceeded;
 use App\Events\UserPurchasedSubscription;
 use Laravel\Cashier\Http\Controllers\WebhookController as CashierController;
 
@@ -19,8 +20,35 @@ class StripeWebhookController extends CashierController
         return $response;
     }
 
-    public function handlePaymentIntentSucceeded()
+    protected function handleCustomerUpdated(array $payload)
     {
-        
+        $response = parent::handleCustomerUpdated($payload);
+
+        if ($user = $this->getUserByStripeId($payload['data']['object']['id'])) {
+            $user->stripe_balance = $payload['data']['object']['balance'];
+            $user->save();
+        }
+
+        return $response;
+    }
+
+
+    protected function handlePaymentIntentSucceeded(array $payload)
+    {
+        if ($user = $this->getUserByStripeId($payload['data']['object']['customer'])) {
+            $data = $payload['data']['object'];
+
+            $paymentData = [
+                'user_id' => $user->id,
+                'source' => 'stripe',
+                'transaction_id' => $data['id'],
+                'amount' => $data['amount'],
+                'currency' => $data['currency'],
+            ];
+
+            PaymentSucceeded::dispatch($paymentData);
+        }
+
+        return $this->successMethod();
     }
 }
