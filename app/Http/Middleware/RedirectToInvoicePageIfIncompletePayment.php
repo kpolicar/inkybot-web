747e24d3289492f615bd29ea3b0a8400c8df4c10
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-08 11:52:40 +0200

    group routes by middleware for plan exists, can purchase subscription

--- app/Http/Middleware/RedirectToInvoicePageIfIncompletePayment.php
+++ app/Http/Middleware/RedirectToInvoicePageIfIncompletePayment.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class SubscriptionNotIncomplete
+class RedirectToInvoicePageIfIncompletePayment
 {
     /**
      * Handle an incoming request.
