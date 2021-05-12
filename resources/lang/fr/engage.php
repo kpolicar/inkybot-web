b279f192644f02f555e7832813b34b64bafd348b
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-12 10:06:11 +0200

    engage show arrow at hall of fame title so it's clear it is a link

--- resources/lang/fr/engage.php
+++ resources/lang/fr/engage.php
@@ -3,6 +3,6 @@
 return [
 
     'heading' => 'Hall of Fame',
-    'subheading' => 'Découvrez les histoires de réussite dans le <a href=":link" class="font-bold uppercase hover:underline">Hall of Fame</a> et voyez ce qu\'Inkybot apporte à la table.<br>Faire des kamas sans effort.',
+    'subheading' => 'Découvrez les histoires de réussite dans le <a href=":link" target="_blank" class="font-bold uppercase hover:underline">Hall of Fame</a> et voyez ce qu\'Inkybot apporte à la table.<br>Faire des kamas sans effort.',
     'download' => 'Télécharger maintenant!',
 ];
