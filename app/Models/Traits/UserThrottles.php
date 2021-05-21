1f92cf9c73c5be832eb1f40b9a202819cbd2ac29
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-21 19:34:43 +0200

    rate limit multiplier x3 to better support old versions, improve deleting access tokens

--- app/Models/Traits/UserThrottles.php
+++ app/Models/Traits/UserThrottles.php
@@ -1,25 +1,26 @@
 <?php namespace App\Models\Traits;
 
 
+// Todo: Remove x3 multipliers (this is meant for old versions)
 trait UserThrottles
 {
     public function getPublishRateLimitPerHourAttribute()
     {
-        return 3 * optional($this->subscription())->quantity ?? 1; // 3 per hour for each subscription
+        return 3 * 3 * optional($this->subscription())->quantity ?? 1; // 3 per hour for each subscription
     }
 
     public function getNotificationRateLimitPerMinuteAttribute()
     {
-        return 5 * optional($this->subscription())->quantity ?? 1; // 5 per minute for each subscription
+        return 3 * 5 * optional($this->subscription())->quantity ?? 1; // 5 per minute for each subscription
     }
 
     public function getCreateStatisticsRateLimitPerMinuteAttribute()
     {
-        return 6 * optional($this->subscription())->quantity ?? 1; // 6 per minute for each subscription
+        return 3 * 6 * optional($this->subscription())->quantity ?? 1; // 6 per minute for each subscription
     }
 
     public function getViewStatisticsRateLimitPerMinuteAttribute()
     {
-        return 15 * optional($this->subscription())->quantity ?? 1; // 15 per minute for each subscription
+        return 3 * 15 * optional($this->subscription())->quantity ?? 1; // 15 per minute for each subscription
     }
 }
