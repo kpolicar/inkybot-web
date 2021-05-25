a0d308f7d6ecb58da2b8955c69826bc3f128cb00
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-25 17:48:18 +0200

    oauth v3, translation fixes

--- resources/lang/fr/profile.php
+++ resources/lang/fr/profile.php
@@ -32,7 +32,7 @@ return [
     'subscription_ends' => 'Votre abonnement prend fin le :date',
     'next_invoice' => 'Votre prochaine facture sera sur le :date',
 
-    'history' => 'Histoire',
+    'history' => 'Historique',
 
     'billing_complete_payment' => 'Terminer le paiement',
     'billing_verify_email' => 'Vous devez d\'abord vérifier votre adresse e-mail.',
@@ -41,7 +41,7 @@ return [
     'credit' => 'crédit',
     'exo_mages_left_in_plan' => 'Il vous reste <b>:number exos</b> dans votre plan',
     'exo_mages_empty' => 'Vos mages exo réussis apparaîtront ici.',
-    'exo_mages' => 'Jusqu\'à présent, vous avez réussi à maged :number exos',
+    'exo_mages' => 'Jusqu\'à présent, vous avez réalisé :number exos',
 
     'statistics_chart_title' => '# Runes utilisées pendant la session',
     'statistics_chart_tip' => 'Ce diagramme peut vous aider à comprendre combien de chaque rune acheter',
