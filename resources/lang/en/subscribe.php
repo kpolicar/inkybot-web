8e3b7957065dea0f7bb29bc8feb85737431bcbb7
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-27 10:57:52 +0200

    remove +1 day extra on crypto payments

--- resources/lang/en/subscribe.php
+++ resources/lang/en/subscribe.php
@@ -3,7 +3,7 @@
 return [
     'heading' => 'How would you like to pay?',
     'subheading' => 'Payment method',
-    'description' => 'Select a payment option below.<br> Cryptocurrency payments are encouraged and will grant you <strong>+1 day</strong> of subscription.',
+    'description' => 'Select a payment option below.',
     'method_card' => 'Card',
     'method_crypto' => 'Crypto',
     'method_crypto_badge' => '+1 day',
