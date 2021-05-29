14e9c3598775fa4a0fca0cad7b469a726e4c63d9
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-29 22:30:06 +0200

    palette.js no longer through webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -22,6 +22,9 @@ mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/profile.js', 'public/js')
     .version()
 
+    .copy('node_modules/google-palette/palette.js', 'public/js')
+    .version()
+
     .sass('resources/sass/app.scss', 'public/css')
     .version()
 
