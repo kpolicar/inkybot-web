8148599ad408dcdf51c87f613d1c13eb29c29613
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-30 12:52:42 +0200

    french translations for subscribe page recurring payment & remove crypto bonus

--- resources/lang/fr/subscribe.php
+++ resources/lang/fr/subscribe.php
@@ -3,8 +3,9 @@
 return [
     'heading' => 'Comment voulez vous payer?',
     'subheading' => 'Moyen de payement',
-    'description' => 'Choisissez un moyen de payement ci-dessous<br> Les payements en cryptomonnaies sont recommandés et vous donneront <strong>+1 jour</strong> d\'abonnement.',
+    'description' => 'Choisissez un moyen de payement ci-dessous.',
     'method_card' => 'Carte',
     'method_crypto' => 'Crypto',
-    'method_crypto_badge' => '+1 jour',
+    'recurring' => 'Abonnement récurrent',
+    'recurring_description' => 'Si elle est sélectionnée, votre carte sera automatiquement facturée mensuellement à la fin de votre période de facturation. Vous pouvez changer cela à tout moment.',
 ];
