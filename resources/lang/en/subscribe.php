af87758e16b35cd1587e64867c0c7971502f6991
Klemen Poličar <klemen.janez.policar@gmail.com>
2021-05-30 12:57:23 +0200

    translate subscription pack

--- resources/lang/en/subscribe.php
+++ resources/lang/en/subscribe.php
@@ -6,6 +6,7 @@ return [
     'description' => 'Select a payment option below.',
     'method_card' => 'Card',
     'method_crypto' => 'Crypto',
+    'package' => 'Subscription pack',
     'recurring' => 'Recurring subscription',
     'recurring_description' => 'If selected, your card will automatically be billed monthly at the end of your billing period. You can change this at any time.',
 ];
