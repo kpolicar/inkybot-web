5add1eeef2403677af3b2d0cf31cd5ac129671db
Klemen Poličar <klemen.janez.policar@gmail.com>
2022-11-28 12:01:27 +0000

    add frameguard to web routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,6 +32,7 @@ class Kernel extends HttpKernel
         'web' => [
             \App\Http\Middleware\RedirectFromPublicUriMiddleware::class,
             \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Http\Middleware\FrameGuard::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
