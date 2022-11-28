322c1ce4253f4f4a4af26ed8ed3e4ff07b5cfb73
Klemen Poličar <klemen.janez.policar@gmail.com>
2022-11-28 11:56:33 +0000

    december crypto promo until 2023-01-02

--- resources/lang/en/subscribe.php
+++ resources/lang/en/subscribe.php
@@ -6,7 +6,7 @@ return [
     'description' => 'Select a payment option below.',
     'description_promotion_title' => 'Promo',
     'description_promotion' => 'Cryptocurrency payments are strongly encouraged and will grant you <strong class="text-red-600">+7 extra days of subscription (+25%)</strong>.',
-    'description_promotion_expires' => 'This offer expires on <strong class="text-green-300">November 1st, 2022</strong>.',
+    'description_promotion_expires' => 'This offer expires on <strong class="text-green-300">January 2nd, 2023</strong>.',
     'method_card' => 'Card',
     'method_crypto' => 'Crypto',
     'method_crypto_badge' => '+7 days',
--- resources/lang/fr/subscribe.php
+++ resources/lang/fr/subscribe.php
@@ -6,7 +6,7 @@ return [
     'description' => 'Choisissez un moyen de payement ci-dessous.',
     'description_promotion_title' => 'Promo',
     'description_promotion' => 'Les paiements en crypto-monnaie sont fortement encouragés et vous accorderont <strong class="text-red-600">+7 jours d\'abonnement supplémentaires (+25%)</strong>.',
-    'description_promotion_expires' => 'Cette offre expire le <strong class="text-green-300">1 novembre 2022</strong>.',
+    'description_promotion_expires' => 'Cette offre expire le <strong class="text-green-300">2 janvier 2023</strong>.',
     'method_card' => 'Carte',
     'method_crypto' => 'Crypto',
     'method_crypto_badge' => '+7 jours',
