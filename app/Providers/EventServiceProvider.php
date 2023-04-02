deadd1de832995bbd683659c7fdcd381c92cb603
Klemen Poličar <klemen.janez.policar@gmail.com>
2023-04-02 17:27:40 +0100

    postmark

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -23,6 +23,7 @@ use Illuminate\Auth\Events\Authenticated;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Mail;
 use Laravel\Cashier\Subscription;
 use Laravel\Passport\Events\AccessTokenCreated;
 
