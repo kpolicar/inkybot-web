71ca3e679cdfa3c402c4db5b5ce204d5f9792902
Klemen Poličar <klemen.janez.policar@gmail.com>
2026-02-26 11:25:50 +0100

    docker

--- discordapp/index.php
+++ discordapp/index.php
@@ -17,7 +17,7 @@ const BOT_USER_ID = 859774039269834782;
 const REACTION_MESSAGE_ID = 859776364697485312;
 
 $discord = new \Discord\Discord([
-    'token' => '***REMOVED***',
+    'token' => getenv('DISCORD_BOT_TOKEN'),
     'loadAllMembers' => true,
 ]);
 
