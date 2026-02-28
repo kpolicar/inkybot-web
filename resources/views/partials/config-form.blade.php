a833d593cb6b8955706704abc65817e27459d651
Klemen Poličar <klemen.janez.policar@gmail.com>
2026-02-28 03:51:27 +0100

    a bunch of popups - updated features section to explain how to use the bot, the different forms

--- resources/views/partials/config-form.blade.php
+++ resources/views/partials/config-form.blade.php
@@ -78,12 +78,20 @@
     .config-window .controls-group { display: flex; align-items: center; justify-content: space-between; }
     .config-window .control-item { display: flex; align-items: center; gap: 8px; }
 
-    .config-window .dropdown {
-        background-color: #080808; color: #EFEFEF;
-        border: 1px solid #080808; padding: 3px; font-size: 9px;
-        display: flex; justify-content: space-between; align-items: center;
+    .config-window .custom-dropdown {
+        display: flex; align-items: stretch;
+        background-color: #080808;
+        height: 19px;
     }
-    .config-window .dropdown::after { content: '▾'; font-size: 8px; color: #999; margin-left: 4px; }
+    .config-window .custom-dropdown-text {
+        flex: 1; padding: 0 5px; font-size: 9px;
+        display: flex; align-items: center; color: #EFEFEF;
+    }
+    .config-window .custom-dropdown-arrow {
+        background-color: #FFFFFF; color: #000000;
+        width: 16px; display: flex; justify-content: center;
+        align-items: center; font-size: 7px; flex-shrink: 0;
+    }
 
     .config-window .num-input {
         background-color: #080808; color: #EFEFEF;
@@ -124,7 +132,9 @@
 <div class="config-window">
     <div class="title-bar">
         <div class="title">
-            <svg class="title-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#000" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/></svg>
+            <svg class="title-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 34">
+                <path fill="#000" transform="translate(3 0)" d="M 25.875 0.0625 C 7.531 0.0625 8.78125 11.4375 8.78125 11.4375 C 8.2290994 12.246696 7.6995693 13.046426 7.25 13.84375 C 7.2192731 13.898245 7.1865288 13.94563 7.15625 14 L 4 14 A 1.0001 1.0001 0 0 0 3.90625 14 A 1.001098 1.001098 0 0 0 3.5 15.90625 L 3 16 C 1.343 16 0 17.344 0 19 L 0 23 C 0 24.656 1.343 26 3 26 L 13 26 C 14.657 26 16 24.656 16 23 L 16 19 C 16 17.344 14.657 16 13 16 L 12.46875 15.875 A 1.0001 1.0001 0 0 0 12 14 L 8.78125 14 C 9.3768313 12.98664 10.046844 11.978044 10.8125 10.96875 C 10.850157 10.925536 10.924518 10.804345 10.96875 10.75 C 11.312321 10.305889 11.682667 9.871631 12.0625 9.4375 C 12.206935 9.262417 12.364614 9.0972071 12.53125 8.90625 C 14.065329 7.2428175 15.90652 5.6997788 18.0625 4.40625 C 16.2125 6.10525 13.15425 9.5635 11.65625 12.1875 C 14.10025 12.4015 17.5465 11.016 19.9375 9 C 19.3975 8.939 16.875 8.48475 16.125 7.84375 C 17.563 7.95975 19.95325 7.97425 20.90625 7.90625 C 22.84525 6.47025 25.063 3.0785 25.875 0.0625 z"/>
+            </svg>
             Inkybot - Config
         </div>
         <div class="controls">
@@ -185,7 +195,10 @@
             <div class="controls-group">
                 <div class="control-item">
                     Automatic shutdown when bot stops
-                    <div class="dropdown" style="width: 70px;">Disabled</div>
+                    <div class="custom-dropdown" style="width: 70px;">
+                        <div class="custom-dropdown-text">Disabled</div>
+                        <div class="custom-dropdown-arrow">▼</div>
+                    </div>
                 </div>
                 <div class="control-item">
                     Custom OCR Image Resize Ratio<br>(leave at "1" unless stats aren't recognized correctly)
@@ -210,8 +223,13 @@
         </div>
         <div class="footer-right">
             <div class="item-dropdown-group">
-                <div class="dropdown" style="width: 150px;">Levitrof Wedding Ring</div>
-                <div class="dropdown" style="width: 20px;"></div>
+                <div class="custom-dropdown" style="width: 150px;">
+                    <div class="custom-dropdown-text">Levitrof Wedding Ring</div>
+                    <div class="custom-dropdown-arrow">▼</div>
+                </div>
+                <div class="custom-dropdown" style="width: 20px;">
+                    <div class="custom-dropdown-arrow">▼</div>
+                </div>
                 <div class="trash-icon">🗑️</div>
             </div>
             <div class="button save">SAVE AS PRESET</div>
